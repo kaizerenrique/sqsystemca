3d7574bc1b92d48df050396954d77fb309baf97e
kaizerenrique <kayserenrique@gmail.com>
2022-02-21 09:48:39 -0400

    correccion de duplicado

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -23,13 +23,6 @@
                     </x-jet-nav-link>                    
                 </div>
 
-
-                                    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                        <x-jet-nav-link href="{{ route('personas') }}" :active="request()->routeIs('personas')">
-                            {{ __('Personas') }}
-                        </x-jet-nav-link>                    
-                    </div>
-
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
