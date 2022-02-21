b8cdb9e1da4e7b2d37e61efaa4a1da5a37bf4985
kaizerenrique <kayserenrique@gmail.com>
2022-02-21 10:27:26 -0400

    dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,9 +9,22 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4 py-4">
                 <div class="col-span-6 sm:col-span-4">
-                    <h2>Bienvenidos !</h2>
-                    <br>
-                    <p>Esta es la pagina de ingreso del sistema.</p>                  
+
+                    <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+                        <div>
+                            <img class="h-16 w-16 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                        </div>
+
+                        <div class="mt-8 text-2xl">
+                            Bienvenido/a {{ Auth::user()->name }}
+                        </div>
+
+                        <div class="mt-6 text-gray-500">
+                            
+                        </div>
+                    </div>
+                                
+                                          
                 </div>
             </div>
         </div>
